525ae123d515f01a6922f80c5f81abc6edcd1c9c
GenkoGenkov <werto.haho@gmail.com>
2021-08-20 14:20:54 +0300

    Adding

---
+++ 1. Programming-Basics/1. First-Steps-In-Coding-Lab/04. Inches-to-Centimeters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main()
+        {
+            double inch = double.Parse(Console.ReadLine());
+
+            double sant = 2.54;
+
+            double sum = inch * sant;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
