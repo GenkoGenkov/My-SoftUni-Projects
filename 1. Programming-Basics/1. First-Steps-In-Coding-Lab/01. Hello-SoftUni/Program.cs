9b4987b66881ea3a8fb4c620dd6d863e07fd81fb
Генко Генков <89202298+GenkoGenkov@users.noreply.github.com>
2021-08-20 13:24:03 +0300

    Add files via upload

---
+++ 1. Programming-Basics/1. First-Steps-In-Coding-Lab/01. Hello-SoftUni/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
