7e412d0c0ec4693c4964751978d8293b83b23a2a
GenkoGenkov <werto.haho@gmail.com>
2021-09-06 17:42:54 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/03. Basic-Syntax,Conditional-Statements-and-Loops-More-Exercise/04. Reverse-String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string password = string.Join("", input.Reverse());
+
+            Console.WriteLine(password);
+        }
+    }
+}
+
