bc4189802c83d374012a8bd6b2766634dc909bce
GenkoGenkov <werto.haho@gmail.com>
2021-09-13 13:44:14 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/09. Arrays-More-Exercise/02. Pascal-Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var pascalTriangle = new long[lines][];
+
+            for (var row = 0; row < lines; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                pascalTriangle[row][0] = 1;
+                pascalTriangle[row][^1] = 1;
+
+                for (var col = 1; col < row; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+            }
+
+            for (var row = 0; row < lines; row++)
+            {
+                Console.WriteLine(string.Join(" ", pascalTriangle[row]));
+            }
+        }
+    }
+}
+
