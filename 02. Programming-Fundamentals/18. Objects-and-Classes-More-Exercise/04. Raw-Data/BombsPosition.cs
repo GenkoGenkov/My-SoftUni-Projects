796b7826f0599e5a19fab91e85a759801e0171b2
GenkoGenkov <werto.haho@gmail.com>
2021-09-22 20:02:10 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/18. Objects-and-Classes-More-Exercise/04. Raw-Data/BombsPosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BombsPosition
+    {
+        public BombsPosition(int row, int col)
+        {
+            Row = row;
+            Col = col;
+            
+        }
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public override string ToString()
+        {
+            return $"{Row},{Col}";
+        }
+    }
+}
