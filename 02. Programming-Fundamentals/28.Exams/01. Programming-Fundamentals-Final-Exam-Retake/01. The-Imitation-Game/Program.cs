300bda33a5714234aa864cad3420f8f2407b04da
GenkoGenkov <werto.haho@gmail.com>
2021-10-04 12:03:07 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/28.Exams/01. Programming-Fundamentals-Final-Exam-Retake/01. The-Imitation-Game/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder message = new StringBuilder(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "Decode")
+            {
+                string[] data = command.Split("|");
+
+                string action = data[0];
+
+                if (action == "Move")
+                {
+                    string moves = data[1];
+
+                    string deleted = message.ToString().Substring(0, int.Parse(moves));
+                    message = message.Remove(0, int.Parse(moves));
+                    message.Append(deleted);
+
+                }
+                else if (action == "Insert")
+                {
+                    int position = int.Parse(data[1]);
+                    string word = data[2];
+
+                    message.Insert(position, word);
+                }
+                else if (action == "ChangeAll")
+                {
+                    string word = data[1];
+                    string replacement = data[2];
+
+                    message.Replace(word, replacement);
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The decrypted message is: {message}");
+        }
+    }
+}
---
+++ 02. Programming-Fundamentals/28.Exams/01. Programming-Fundamentals-Final-Exam-Retake/03. The-Pianist/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, PianoPeace> collection = new Dictionary<string, PianoPeace>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] piecesData = Console.ReadLine().Split("|");
+
+                string piece = piecesData[0];
+                string composer = piecesData[1];
+                string key = piecesData[2];
+
+                PianoPeace currentPianoPiece = new PianoPeace(composer, key);
+
+                collection.Add(piece, currentPianoPiece);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "Stop")
+            {
+                string[] dataActions = command.Split("|");
+
+                string action = dataActions[0];
+
+                if (action == "Add")
+                {
+                    string pieceName = dataActions[1];
+                    string composerName = dataActions[2];
+                    string keyName = dataActions[3];
+
+                    if (!collection.ContainsKey(pieceName))
+                    {
+                        collection.Add(pieceName, new PianoPeace(composerName, keyName));
+
+                        Console.WriteLine($"{pieceName} by {composerName} in {keyName} added to the collection!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{pieceName} is already in the collection!");
+                    }
+
+                }
+                else if (action == "Remove")
+                {
+                    string pieceName = dataActions[1];
+
+                    if (collection.ContainsKey(pieceName))
+                    {
+                        collection.Remove(pieceName);
+
+                        Console.WriteLine($"Successfully removed {pieceName}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {pieceName} does not exist in the collection.");
+                    }
+                }
+                else if (action == "ChangeKey")
+                {
+                    string pieceName = dataActions[1];
+                    string keyName = dataActions[2];
+
+                    if (collection.ContainsKey(pieceName))
+                    {
+                        collection[pieceName].Key = keyName;
+
+                        Console.WriteLine($"Changed the key of {pieceName} to {keyName}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid operation! {pieceName} does not exist in the collection.");
+                    }
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var item in collection.OrderBy(i => i.Key).ThenBy(i => i.Value.Composer))
+            {
+                Console.WriteLine($"{item.Key} -> Composer: {item.Value.Composer}, Key: {item.Value.Key}");
+            }
+
+        }
+    }
+
+    class PianoPeace
+    {
+        public PianoPeace(string composer, string key)
+        {
+            Composer = composer;
+            Key = key;
+        }
+
+        public string Composer { get; set; }
+        public string Key { get; set; }
+    }
+}
+
