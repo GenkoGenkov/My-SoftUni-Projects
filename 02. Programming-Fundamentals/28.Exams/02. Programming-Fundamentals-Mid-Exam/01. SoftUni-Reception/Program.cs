dd301ea2b0eb4415f3016daac4a94757a95c7910
GenkoGenkov <werto.haho@gmail.com>
2021-10-05 09:59:18 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/28.Exams/02. Programming-Fundamentals-Mid-Exam/01. SoftUni-Reception/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+
+            int allEmployeeEfficiency = firstEmployee + secondEmployee + thirdEmployee;
+
+            int hours = 0;
+
+            while (students > 0)
+            {
+                students -= allEmployeeEfficiency;
+                hours++;
+
+                if (hours % 4 == 0 && hours >= 4)
+                {
+                    hours++;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
+
