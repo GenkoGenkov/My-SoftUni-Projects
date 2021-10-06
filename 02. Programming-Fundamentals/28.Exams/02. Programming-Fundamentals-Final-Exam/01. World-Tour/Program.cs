88da2b0f41c5ba502c73d044f2a9ae7d6c3e1750
GenkoGenkov <werto.haho@gmail.com>
2021-10-06 11:53:07 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/28.Exams/02. Programming-Fundamentals-Final-Exam/01. World-Tour/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder countries = new StringBuilder(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "Travel")
+            {
+                string[] information = command.Split(":");
+
+                string action = information[0];
+
+                if (action == "Add Stop")
+                {
+                    int index = int.Parse(information[1]);
+                    string destination = information[2];
+
+                    if (index <= countries.Length - 1 && index >= 0)
+                    {
+                        countries.Insert(index, destination);
+                    }
+
+                    Console.WriteLine(string.Join("", countries));
+                }
+                else if (action == "Remove Stop")
+                {
+                    int start = int.Parse(information[1]);
+                    int end = int.Parse(information[2]);
+
+                    if (start >= 0 && start <= countries.Length - 1 && end <= countries.Length - 1 && end >= 0)
+                    {
+                        countries.Remove(start, end - start + 1);
+                    }
+
+                    Console.WriteLine(string.Join("", countries));
+                }
+                else if (action == "Switch")
+                {
+                    string old = information[1];
+                    string @new = information[2];
+
+                    if (countries.ToString().Contains(old) && old != @new)
+                    {
+                        countries.Replace(old, @new);
+                    }
+
+                    Console.WriteLine(string.Join("", countries));
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {string.Join("", countries)}");
+        }
+    }
+}
+
---
+++ 02. Programming-Fundamentals/28.Exams/02. Programming-Fundamentals-Final-Exam/02. Destination-Mapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([=|\/])(?<locations>[A-Z][A-Za-z]{2,})\1";
+
+            string input = Console.ReadLine();
+
+            MatchCollection countries = Regex.Matches(input, pattern);
+
+            Console.WriteLine($"Destinations: {string.Join(", ", countries.Select(g => g.Groups["locations"].Value))}");
+            Console.WriteLine($"Travel Points: {string.Join("", countries.Select(g => g.Groups["locations"].Value)).Length}");
+        }
+    }
+}
+
