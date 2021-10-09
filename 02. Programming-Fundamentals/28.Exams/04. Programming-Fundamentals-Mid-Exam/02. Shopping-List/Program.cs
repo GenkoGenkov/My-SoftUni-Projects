1b94998f575af71c8b7067313c7aa4acd612430b
GenkoGenkov <werto.haho@gmail.com>
2021-10-09 09:41:59 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/28.Exams/04. Programming-Fundamentals-Mid-Exam/02. Shopping-List/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = Console.ReadLine().Split("!").ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "Go Shopping!")
+            {
+                string[] data = command.Split();
+
+                string action = data[0];
+                string item = data[1];
+
+                if (action == "Urgent")
+                {
+                    if (!shoppingList.Contains(item))
+                    {
+                        shoppingList.Insert(0, item);
+                    }
+                }
+                else if (action == "Unnecessary")
+                {
+                    if (shoppingList.Contains(item))
+                    {
+                        shoppingList.Remove(item);
+                    }
+                }
+                else if (action == "Correct")
+                {
+                    if (shoppingList.Contains(data[1]))
+                    {
+                        int index = shoppingList.FindIndex(s => s == data[1]);
+                        shoppingList.RemoveAt(index);
+                        shoppingList.Insert(index, data[2]);
+                    }
+                }
+                else if (action == "Rearrange")
+                {
+                    if (shoppingList.Contains(item))
+                    {
+                        int index = shoppingList.FindIndex(s => s == item);
+                        shoppingList.RemoveAt(index);
+                        shoppingList.Add(item);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", shoppingList));
+        }
+    }
+}
+
---
+++ 02. Programming-Fundamentals/28.Exams/04. Programming-Fundamentals-Mid-Exam/03. Heart-Delivery/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = Console.ReadLine().Split("@").Select(int.Parse).ToList();
+            string input = Console.ReadLine();
+
+            int currentIndex = 0;
+            int initialIndex = 0;
+
+            while (input != "Love!")
+            {
+                string[] jump = input.Split();
+
+                currentIndex += int.Parse(jump[1]);
+
+                if (currentIndex > integers.Count - 1)
+                {
+                    currentIndex = 0;
+                }
+
+                if (integers[currentIndex] == 0)
+                {
+                    Console.WriteLine($"Place {currentIndex} already had Valentine's day.");
+
+                    initialIndex = currentIndex;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                integers[currentIndex] -= 2;
+                initialIndex = currentIndex;
+
+                if (integers[currentIndex] == 0)
+                {
+                    Console.WriteLine($"Place {currentIndex} has Valentine's day.");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Cupid's last position was {currentIndex}.");
+
+            if (integers.All(i => i == 0))
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid has failed {integers.Count(i => i > 0)} places.");
+            }
+        }
+    }
+}
+
