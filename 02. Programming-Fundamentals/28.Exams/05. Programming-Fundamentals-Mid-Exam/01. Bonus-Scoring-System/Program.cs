15bf9fbb7ca3e42893fef27a16705e0ee64ac638
GenkoGenkov <werto.haho@gmail.com>
2021-10-12 14:48:50 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/28.Exams/05. Programming-Fundamentals-Mid-Exam/01. Bonus-Scoring-System/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double studentCount = double.Parse(Console.ReadLine());
+            double lecturesCount = double.Parse(Console.ReadLine());
+            double initialBonus = double.Parse(Console.ReadLine());
+
+            double maxStudentBonus = 0;
+            double maxStudentAttendance = 0;
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                double studentAttendance = double.Parse(Console.ReadLine());
+
+                double totalBonus = studentAttendance / lecturesCount * (5 + initialBonus);
+
+                if (totalBonus > maxStudentBonus)
+                {
+                    maxStudentBonus = totalBonus;
+                    maxStudentAttendance = studentAttendance;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(maxStudentBonus)}.");
+            Console.WriteLine($"The student has attended {maxStudentAttendance} lectures.");
+        }
+    }
+}
---
+++ 02. Programming-Fundamentals/28.Exams/05. Programming-Fundamentals-Mid-Exam/03. Inventory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> inventory = Console.ReadLine().Split(", ").ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "Craft!")
+            {
+                string[] commands = input.Split(" - ");
+
+                switch (commands[0])
+                {
+                    case "Collect":
+
+                        if (!inventory.Contains(commands[1]))
+                        {
+                            inventory.Add(commands[1]);
+                        }
+
+                        break;
+                    case "Drop":
+
+                        if (inventory.Contains(commands[1]))
+                        {
+                            inventory.Remove(commands[1]);
+                        }
+
+                        break;
+                    case "Combine Items":
+
+                        string[] items = commands[1].Split(":");
+
+                        if (inventory.Contains(items[0]))
+                        {
+                            int index = inventory.FindIndex(s => s == items[0]);
+                            inventory.Insert(index + 1, items[1]);
+                        }
+
+                        break;
+                    case "Renew":
+
+                        if (inventory.Contains(commands[1]))
+                        {
+                            int indexRenew = inventory.FindIndex(i => i == commands[1]);
+                            string item = inventory[indexRenew];
+                            inventory.RemoveAt(indexRenew);
+                            inventory.Add(item);
+                        }
+
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", inventory));
+        }
+    }
+}
