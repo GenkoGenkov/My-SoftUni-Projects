c70f132c94e71af5347e69b7d7f5642a9eaaf69c
GenkoGenkov <werto.haho@gmail.com>
2021-10-14 11:24:32 +0300

    Adding new solutions

---
+++ 02. Programming-Fundamentals/28.Exams/06. Programming-Fundamentals-Mid-Exam-Retake/01. Black-Flag/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int daysPirating = int.Parse(Console.ReadLine());
+            int plunderPerDay = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double currentPlunder = 0;
+
+            for (int i = 1; i <= daysPirating; i++)
+            {
+                currentPlunder += plunderPerDay;
+
+                if (i % 3 == 0 && i >= 3)
+                {
+                    currentPlunder += (double)plunderPerDay / 2;
+                }
+
+                if (i % 5 == 0 && i >= 5)
+                {
+                    currentPlunder *= 0.70;
+                }
+            }
+
+            if (currentPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {currentPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {currentPlunder / expectedPlunder * 100:f2}% of the plunder.");
+            }
+        }
+    }
+}
