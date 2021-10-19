53bef8879057d8eebc9691d6771d9bd43085fa7a
GenkoGenkov <werto.haho@gmail.com>
2021-10-19 11:23:50 +0300

    Adding new files

---
+++ 03. Programming-Advanced/05. Multidimensional-Arrays-Exercise/2. Squares-in-Matrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeOfMatrix = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = sizeOfMatrix[0];
+            int cols = sizeOfMatrix[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int squares = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        squares++;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(squares);
+        }
+    }
+}
+
---
+++ 03. Programming-Advanced/05. Multidimensional-Arrays-Exercise/5. Snake-Moves/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeOfmatrix = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int rows = sizeOfmatrix[0];
+            int cols = sizeOfmatrix[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            string snake = Console.ReadLine();
+
+            bool isLetfOrRight = true;
+            int counter = 0;
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (isLetfOrRight)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        matrix[row, col] = snake[counter++];
+
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+
+                    isLetfOrRight = false;
+                }
+                else
+                {
+                    for (int col = matrix.GetLength(1) - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[counter++];
+
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+
+                    isLetfOrRight = true;
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
