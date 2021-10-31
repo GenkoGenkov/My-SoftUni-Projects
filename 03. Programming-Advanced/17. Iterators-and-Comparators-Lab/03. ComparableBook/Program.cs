cc481ecb2710dd7485eaf3389d105cd0fb242e7d
GenkoGenkov <werto.haho@gmail.com>
2021-10-31 12:07:11 +0200

    Adding new solutions

---
+++ 03. Programming-Advanced/17. Iterators-and-Comparators-Lab/03. ComparableBook/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+
+    public class Book : IComparable<Book>
+    {
+        private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.authors = new List<string>(authors);
+        }
+
+        public string Title { get; private set; }
+
+        public int Year { get; private set; }
+
+        public IReadOnlyCollection<string> Authors
+        {
+            get
+            {
+                return this.authors.AsReadOnly();
+            }
+        }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year.CompareTo(other.Year) != 0)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+
+            return this.Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[this.currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.currentIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+        }
+    }
+}
---
+++ 03. Programming-Advanced/17. Iterators-and-Comparators-Lab/04. BookComparer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in libraryTwo)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public IReadOnlyList<string> Authors { get; private set; }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year != other.Year)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+
+            return this.Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+    public class Library : IEnumerable<Book>
+    {
+        public readonly SortedSet<Book> books;
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private SortedSet<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                this.books = new SortedSet<Book>(books, new BookComparator());
+            }
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+
+            public Book Current => books.ToList()[currentIndex];
+
+            object? IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+        }
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books, new BookComparator());
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            if (x.Title != y.Title)
+            {
+                return x.Title.CompareTo(y.Title);
+            }
+
+            return y.Year.CompareTo(x.Year);
+        }
+    }
+}
