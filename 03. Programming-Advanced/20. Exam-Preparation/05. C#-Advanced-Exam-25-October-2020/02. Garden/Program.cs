2ca893aa11e5e188da35bc5d3048776982cd6426
GenkoGenkov <werto.haho@gmail.com>
2021-11-07 18:47:45 +0200

    Adding new solutions

---
+++ 03. Programming-Advanced/20. Exam-Preparation/05. C#-Advanced-Exam-25-October-2020/02. Garden/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split();
+
+            int isRow = int.Parse(size[0]);
+            int isCol = int.Parse(size[1]);
+
+            var garden = new int[isRow, isCol];
+
+            for (int row = 0; row < isRow; row++)
+            {
+                for (int col = 0; col < isCol; col++)
+                {
+                    garden[row, col] = 0;
+                }
+            }
+
+            while (true)
+            {
+                var tokens = Console.ReadLine();
+
+                if (tokens == "Bloom Bloom Plow")
+                {
+                    break;
+                }
+
+                int row = int.Parse(tokens[0].ToString());
+                int col = int.Parse(tokens[2].ToString());
+
+                if (row < 0 || row > garden.GetLength(0) && col < 0 || col > garden.GetLength(1))
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+
+                for (int i = 0; i < garden.GetLength(0); i++)
+                {
+                    garden[row, i]++;
+                }
+
+                for (int j = 0; j < garden.GetLength(1); j++)
+                {
+                    garden[j, col]++;
+                }
+
+                garden[row, col]--;
+            }
+
+            for (int row = 0; row < isRow; row++)
+            {
+                for (int col = 0; col < isCol; col++)
+                {
+                    Console.Write(garden[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ 03. Programming-Advanced/20. Exam-Preparation/05. C#-Advanced-Exam-25-October-2020/03. Classroom/Classroom.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity { get; }
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (Capacity <= Count)
+            {
+                return "No seats in the classroom";
+            }
+
+            students.Add(student);
+
+            return $"Added student {student.FirstName} {student.LastName}"; 
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            bool isStudentExist = students.Exists(x => x.FirstName == firstName && x.LastName == lastName);
+
+            if (isStudentExist)
+            {
+                students.Remove(students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName));
+
+                return $"Dismissed student {firstName} {lastName}";
+            }
+
+            return "Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            var studentSubject = students.Where(x => x.Subject == subject).ToList();
+
+            if (studentSubject.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+
+                foreach (var item in studentSubject)
+                {
+                    sb.AppendLine($"{item.FirstName} {item.LastName}");
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+
+            return "No students enrolled for the subject";
+        }
+
+        public int GetStudentsCount() => Count;
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student currentStudent = students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+            return currentStudent;
+        }
+    }
+}
