2c5efbe5f2b05e58f956a7f4d942a7844acd7b2d
GenkoGenkov <werto.haho@gmail.com>
2021-11-09 21:33:58 +0200

    Adding new solutions

---
+++ 03. Programming-Advanced/20. Exam-Preparation/06. C#-Advanced-Exam-20-February-2021/03. The Race/Race.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> data;
+
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public void Add(Racer racer)
+        {
+            if (Capacity > Count)
+            {
+                data.Add(racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Racer currentRacer = data.FirstOrDefault(x => x.Name == name);
+
+            if (currentRacer == null)
+            {
+                return false;
+            }
+            else
+            {
+                data.Remove(currentRacer);
+                return true;
+            }
+        }
+
+        public Racer GetOldestRacer()
+        {
+            return data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return data.OrderByDescending(x => x.Car.Speed).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Racers participating at {Name}:");
+
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
