db25f0b31482297dd9fd37698208d3fa27bea9b3
GenkoGenkov <werto.haho@gmail.com>
2021-11-15 15:15:37 +0200

    Adding new solutions

---
+++ 04. OOP-C#/04. Inheritance-Exercise/06. Animals/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string input = Console.ReadLine();
+
+            while (input != "Beast!")
+            {
+                string[] animalInfo = Console.ReadLine().Split();
+
+                string name = animalInfo[0];
+                int age = int.Parse(animalInfo[1]);
+                string gender = animalInfo[2];
+
+                if (input == "Cat")
+                {
+                    Cat cat = new Cat(name, age, gender);
+                    animals.Add(cat);
+                }
+                else if (input == "Dog")
+                {
+                    Dog dog = new Dog(name, age, gender);
+                    animals.Add(dog);
+                }
+                else if (input == "Frog")
+                {
+                    Frog frog = new Frog(name, age, gender);
+                    animals.Add(frog);
+                }
+                else if (input == "Kitten")
+                {
+                    Kitten kitten = new Kitten(name, age);
+                    animals.Add(kitten);
+                }
+                else if (input == "Tomcat")
+                {
+                    Tomcat tomcat = new Tomcat(name, age);
+                    animals.Add(tomcat);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in animals)
+            {
+                Console.WriteLine(item.GetType().Name);
+                Console.WriteLine($"{item.Name} {item.Age} {item.Gender}");
+                Console.WriteLine(item.ProduceSound());
+            }
+        }
+    }
+}
