25c0cb5a796ed7f7022c71c7578419c888856564
GenkoGenkov <werto.haho@gmail.com>
2021-11-16 19:49:38 +0200

    Adding new solutions

---
+++ 04. OOP-C#/05. Encapsulation-Lab/01. Persons/Program.cs
@@ -0,0 +1,28 @@
+﻿using PersonsInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split();
+
+                Person person = new Person(line[0], line[1], int.Parse(line[2]));
+                people.Add(person);
+            }
+
+            people.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList().ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
