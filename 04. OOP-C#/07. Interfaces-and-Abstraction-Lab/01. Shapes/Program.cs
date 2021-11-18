886df8ccf2e4480f262d14867c214943196ea095
GenkoGenkov <werto.haho@gmail.com>
2021-11-18 22:45:28 +0200

    Adding new solutions

---
+++ 04. OOP-C#/07. Interfaces-and-Abstraction-Lab/01. Shapes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var radius = int.Parse(Console.ReadLine());
+
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+
+            IDrawable rect = new Rectangle(width, height);
+
+            circle.Draw();
+            rect.Draw();
+        }
+    }
+}
