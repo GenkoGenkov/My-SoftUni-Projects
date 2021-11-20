36821f6631e583180c05c6073766e59ff9af8bef
GenkoGenkov <werto.haho@gmail.com>
2021-11-20 18:49:17 +0200

    Adding new solutions

---
+++ 04. OOP-C#/09. Polymorphism-Lab/03. Shapes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
