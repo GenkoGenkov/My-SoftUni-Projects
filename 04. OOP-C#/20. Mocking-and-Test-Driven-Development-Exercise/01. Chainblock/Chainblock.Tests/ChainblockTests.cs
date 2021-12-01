a0de0c996b4df8e4533e101365ddedba4f688601
GenkoGenkov <werto.haho@gmail.com>
2021-12-01 23:11:55 +0200

    Adding new solutions

---
+++ 04. OOP-C#/20. Mocking-and-Test-Driven-Development-Exercise/01. Chainblock/Chainblock.Tests/ChainblockTests.cs
@@ -0,0 +1,142 @@
+﻿using Chainblock.Contracts;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chainblock.Tests
+{
+    public class ChainblockTests
+    {
+        private IChainblock chainblock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            chainblock = new Chainblock();
+        }
+
+        [Test]
+        public void AddMethodShouldAddUniqueRecords()
+        {
+            ITransaction transaction = new Transaction(
+                456,
+                TransactionStatus.Successfull,
+                "Niki",
+                "Stoyan",
+                50);
+
+            chainblock.Add(transaction);
+
+            bool exist = chainblock.Contains(transaction);
+
+            Assert.True(exist);
+            Assert.AreEqual(1, chainblock.Count);
+        }
+
+        [Test]
+        public void AddMethodMustAddOnlyUniqueTransactions()
+        {
+            ITransaction transaction = new Transaction(
+                12345,
+                TransactionStatus.Successfull,
+                "Niki",
+                "Stoyan",
+                50);
+
+            ITransaction duplicatedTransaction = new Transaction(
+                12345,
+                TransactionStatus.Successfull,
+                "Niki",
+                "Stoyan",
+                50);
+
+            chainblock.Add(transaction);
+            chainblock.Add(duplicatedTransaction);
+
+            bool transactionExist = chainblock.Contains(transaction);
+
+            int expectedCount = 1;
+
+            Assert.IsTrue(transactionExist);
+            Assert.AreEqual(expectedCount, chainblock.Count);
+        }
+
+        [Test]
+        public void ChangeTransactionStatusShouldChangeTransactionStatus()
+        {
+            int transactionId = 85643;
+
+            ITransaction transaction = new Transaction(
+                transactionId,
+                TransactionStatus.Aborted,
+                "Kiro",
+                "Ivo",
+                100);
+
+            chainblock.Add(transaction);
+
+            chainblock.ChangeTransactionStatus(transactionId, TransactionStatus.Failed);
+
+            ITransaction chainblockTransaction = chainblock.GetById(transactionId);
+
+            Assert.AreEqual(TransactionStatus.Failed, chainblockTransaction.Status);
+        }
+
+        [Test]
+        public void ChangeTransactionStatusShouldFailWhenTransactionDoesNotExist()
+        {
+            ITransaction transaction = new Transaction(
+                6325,
+                TransactionStatus.Successfull,
+                "Ivan",
+                "Kiro",
+                34);
+
+            chainblock.Add(transaction);
+
+            Assert.Throws<ArgumentException>(() => chainblock.ChangeTransactionStatus(3333, TransactionStatus.Unauthorized));
+        }
+
+        [Test]
+        public void RemoveTransactionByIdShouldRemoveTransactionIfExist()
+        {
+            ITransaction transaction = new Transaction(
+                1,
+                TransactionStatus.Failed,
+                "Kiro",
+                "Nasko",
+                500);
+
+            chainblock.Add(transaction);
+
+            chainblock.RemoveTransactionById(1);
+
+            bool exist = chainblock.Contains(1);
+
+            Assert.AreEqual(0, chainblock.Count);
+            Assert.IsFalse(exist);
+        }
+
+        [Test]
+        public void RemoveTransactionByIdShouldThrowExceptionIfTransactionDoesNotExist()
+        {
+            ITransaction transaction = new Transaction(
+                1,
+                TransactionStatus.Failed,
+                "Kiro",
+                "Nasko",
+                500);
+
+            chainblock.Add(transaction);
+
+            Assert.Throws<InvalidOperationException>(() => chainblock.RemoveTransactionById(15));
+        }
+
+        [Test]
+        public void GetByIdShouldThrowExceptionIfInvaliIdIsPassed()
+        {
+            Assert.Throws<InvalidOperationException>(() => chainblock.GetById(123));
+        }
+    }    
+}
---
+++ 04. OOP-C#/20. Mocking-and-Test-Driven-Development-Exercise/01. Chainblock/Chainblock/Chainblock.cs
@@ -0,0 +1,124 @@
+﻿using Chainblock.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chainblock
+{
+    public class Chainblock : IChainblock
+    {
+        private readonly Dictionary<int, ITransaction> transactions;
+
+        public Chainblock()
+        {
+            transactions = new Dictionary<int, ITransaction>();
+        }
+
+        public int Count => transactions.Count;
+
+        public void Add(ITransaction tx)
+        {
+            if (Contains(tx))
+            {
+                return;
+            }
+
+            transactions.Add(tx.Id, tx);
+        }
+
+        public void ChangeTransactionStatus(int id, TransactionStatus newStatus)
+        {
+            if (!Contains(id))
+            {
+                throw new ArgumentException();
+            }
+
+            transactions[id].Status = newStatus;
+        }
+
+        public bool Contains(ITransaction tx) => transactions.ContainsKey(tx.Id);
+
+        public bool Contains(int id) => transactions.ContainsKey(id);
+        
+        public IEnumerable<ITransaction> GetAllInAmountRange(double lo, double hi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetAllOrderedByAmountDescendingThenById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITransaction GetById(int id) 
+        {
+            if (!Contains(id))
+            {
+                throw new InvalidOperationException();
+            }
+
+            return transactions[id];
+        }
+
+        public IEnumerable<ITransaction> GetByReceiverAndAmountRange(string receiver, double lo, double hi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByReceiverOrderedByAmountThenById(string receiver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetBySenderAndMinimumAmountDescending(string sender, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetBySenderOrderedByAmountDescending(string sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByTransactionStatus(TransactionStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ITransaction> GetByTransactionStatusAndMaximumAmount(TransactionStatus status, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<ITransaction> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTransactionById(int id)
+        {
+            if (!Contains(id))
+            {
+                throw new InvalidOperationException();
+            }
+
+            transactions.Remove(id);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
