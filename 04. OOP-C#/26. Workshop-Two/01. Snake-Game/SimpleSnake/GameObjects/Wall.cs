083105d1cabca20880b8cb5c3b2d03152141bdc0
GenkoGenkov <werto.haho@gmail.com>
2021-12-08 23:10:44 +0200

    Adding new solutions

---
+++ 04. OOP-C#/26. Workshop-Two/01. Snake-Game/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char WallSymbol = '\u25A0';
+
+        public Wall(int leftX, int topY) 
+            : base(leftX, topY)
+        {
+            InitalizeWall();
+        }
+
+        private void InitalizeWall()
+        {
+
+            for (int leftX = 0; leftX < this.LeftX; leftX++)
+            {
+                this.Draw(leftX, 0, WallSymbol);
+            }
+
+            for (int leftX = 0; leftX < this.LeftX; leftX++)
+            {
+                this.Draw(leftX, this.TopY, WallSymbol);
+            }
+
+            for (int topY = 0; topY < this.TopY; topY++)
+            {
+                this.Draw(0, topY, WallSymbol);
+            }
+
+            for (int topY = 0; topY < this.TopY; topY++)
+            {
+                this.Draw(this.LeftX, topY, WallSymbol);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
