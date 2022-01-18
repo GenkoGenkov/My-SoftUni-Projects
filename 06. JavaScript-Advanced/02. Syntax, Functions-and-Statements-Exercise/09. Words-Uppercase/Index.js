8cb17c70cba374059c8bd6c4c6e6c9af853e2edf
GenkoGenkov <werto.haho@gmail.com>
2022-01-18 21:34:26 +0200

    Adding new solutions

---
+++ 06. JavaScript-Advanced/02. Syntax, Functions-and-Statements-Exercise/09. Words-Uppercase/Index.js
@@ -0,0 +1,7 @@
+function wordsUpperCase(string){
+
+    string = string.toUpperCase();
+    let regex = /[A-Za-z0-9]+/g;
+    strArray = [...string.matchAll(regex)];
+    console.log(strArray.join(", "));
+}
