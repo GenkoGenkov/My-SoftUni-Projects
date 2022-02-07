8e2cf71d083cad1712153bed560a1fae5d4118e1
GenkoGenkov <werto.haho@gmail.com>
2022-02-07 19:12:14 +0200

    Adding new solutions

---
+++ 06. JavaScript-Advanced/11. Advanced-Functions-Lab/01. Area-and-Volume-Calculator/Index.js
@@ -0,0 +1,16 @@
+function solve(area, vol, input) {
+
+    let coordinates = JSON.parse(input);
+    let result = [];
+
+    for (const points of coordinates) {
+
+        result.push({
+
+            area: area.call(points),
+            volume: vol.call(points),
+        });       
+    }
+
+    return result;
+}
---
+++ 06. JavaScript-Advanced/11. Advanced-Functions-Lab/06. List-Processor/Index.js
@@ -0,0 +1,51 @@
+function listProcessor(input) {
+
+    let collection = [];
+
+    const utilityObj = {
+
+        add,
+        remove,
+        print
+
+    }
+
+    function add(element) {
+
+        collection.push(element);
+
+    }
+
+    function remove(element) {
+
+        collection = collection.filter(e => e !== element);
+
+    }
+
+    function print() {
+
+        console.log(collection.join(','));
+
+    }
+
+    input.forEach(command => {
+
+        const cmdArgs = command.split(' ');
+        const cmd = cmdArgs[0];
+        const item = cmdArgs[1];
+
+        switch(cmd) {
+
+            case "add":
+                utilityObj.add(item);
+                break;
+            case "remove":
+                utilityObj.remove(item);
+                break;
+            case "print":
+                utilityObj.print();
+                break;
+        }
+
+    });
+}
