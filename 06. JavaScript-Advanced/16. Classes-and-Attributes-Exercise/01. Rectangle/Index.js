7d642d080e77e648063d9a13685d66f93705c554
GenkoGenkov <werto.haho@gmail.com>
2022-02-12 22:33:00 +0200

    Adding new solutions

---
+++ 06. JavaScript-Advanced/16. Classes-and-Attributes-Exercise/01. Rectangle/Index.js
@@ -0,0 +1,14 @@
+class Rectangle {
+
+    constructor(width, height, color) {
+
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea(w, h) {
+
+        return this.width * this.height;
+    }
+}
