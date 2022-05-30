6a603e1b6cc6b9512e02a535aaafb5115abc753f
Genko Genkov <werto.haho@gmail.com>
2022-05-30 18:59:52 +0300

    Adding new solutions

---
+++ 08. DB-MS-SQL/03. Table-Relations-Exercise/06. University-Database/University Database.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Subjects
+(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Majors
+(
+	MajorID INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber INT,
+	StudentName VARCHAR(50) NOT NULL,
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments
+(
+	PaymentID INT PRIMARY KEY IDENTITY,
+	PaymentDate DATETIME,
+	PaymentAmmount DECIMAL(15, 2) NOT NULL,
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Agenda
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID)
+
+	CONSTRAINT PK_Student_Subject PRIMARY KEY (StudentID, SubjectID)
+)
+
