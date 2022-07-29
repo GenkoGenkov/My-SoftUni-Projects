41eb492728def69c51f957c8056ca6668d109aca
Genko Genkov <werto.haho@gmail.com>
2022-07-29 18:58:35 +0300

    Adding new solutions

---
+++ 09. DB-Entity-Framework-Core/11. Exam-Preparation/01. SoftJail-Retake-Exam-14-August-2020/03. Data Export/DataProcessor/Serializer.cs
@@ -0,0 +1,60 @@
+﻿namespace SoftJail.DataProcessor
+{
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var prisoners = context
+                .Prisoners
+                .ToArray()
+                .Where(p => ids.Contains(p.Id))
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers
+                        .Select(po => new
+                        {
+                            OfficerName = po.Officer.FullName,
+                            Department = po.Officer.Department.Name
+                        })
+                        .OrderBy(o => o.OfficerName)
+                        .ToArray(),
+                    TotalOfficerSalary = Math.Round(p.PrisonerOfficers.Sum(po => po.Officer.Salary), 2)
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+
+            return json;
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            var names = prisonersNames.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var result = context.Prisoners
+                .Include(x => x.Mails)
+                .Where(x => names.Contains(x.FullName))
+                .ProjectTo<PrisonerViewModel>()
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+            var xml = XmlConverter.Serialize(result, "Prisoners");
+
+            return xml;
+        }
+    }
+}
