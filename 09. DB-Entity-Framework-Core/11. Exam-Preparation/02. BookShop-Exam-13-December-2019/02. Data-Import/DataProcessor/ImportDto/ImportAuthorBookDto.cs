3245a90fe09313e1f77aebe58ea6fee8518bd42e
Genko Genkov <werto.haho@gmail.com>
2022-07-30 13:31:25 +0300

    Adding new solutions

---
+++ 09. DB-Entity-Framework-Core/11. Exam-Preparation/02. BookShop-Exam-13-December-2019/02. Data-Import/DataProcessor/ImportDto/ImportAuthorBookDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class ImportAuthorBookDto
+    {
+        [JsonProperty("Id")]
+        public int? BookId { get; set; }
+    }
+}
+
+
+
