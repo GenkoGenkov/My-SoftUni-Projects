3f3529056373063e0f6d345fd713fd8c6664901e
Genko Genkov <werto.haho@gmail.com>
2022-08-02 23:02:46 +0300

    Adding new solutions

---
+++ 09. DB-Entity-Framework-Core/11. Exam-Preparation/03. VaporStore-Exam-08-August-2020/02. Data-Import/DataProcessor/Dto/Import/UserJsonInputModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using VaporStore.Data.Models;
+
+namespace VaporStore.DataProcessor.Dto.Import
+{
+    public class UserJsonInputModel
+    {
+        [Required]
+        [RegularExpression("[A-Z]{1}[a-z]{2,} [A-Z]{1}[a-z]{2,}")]
+        public string FullName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Range(3, 103)]
+        public int Age { get; set; }
+
+        public IEnumerable<CardJsonInputModel> Cards { get; set; }
+    }
+}
