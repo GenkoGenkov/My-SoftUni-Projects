5deb26b0e85c8452d84101a1c7313e0217b84070
GenkoGenkov <werto.haho@gmail.com>
2022-10-17 22:32:28 +0300

    Adding new solutions

---
+++ 10. ASP.NET-Fundamentals/06. Exam-Preparations/01. SMS-C#-Web-Basics-Retake-Exam-24-August-2021/SMS/Data/DatabaseConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Data
+{
+    public class DatabaseConfiguration
+    {
+        // ReSharper disable once InconsistentNaming
+        public const string ConnectionString =
+            @"Server=GI\\SQLEXPRESS;Database=Sms;Trusted_Connection=True;MultipleActiveResultSets=true";
+    }
+}
