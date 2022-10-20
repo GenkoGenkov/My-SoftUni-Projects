481da0fce49593c2774b96d01aa3102992901314
GenkoGenkov <werto.haho@gmail.com>
2022-10-20 12:31:17 +0300

    Adding new solutions

---
+++ 10. ASP.NET-Fundamentals/06. Exam-Preparations/02. Watchlist-Exam-Preparation-17-October-2022/Watchlist/Data/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+namespace Watchlist.Data.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
